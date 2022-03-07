16f31c93f49133964b60e334c08ec03b1c7d8cf6
KeeBoonHwee <2717921k@student.gla.ac.uk>
2022-03-07 10:41:19 +0800

    First commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+
+# In VS Code clone github branch via 
+# git clone -b KeeBoonHwee https://github.com/tlengz/CSC1010TeamProject.git
+
+def main():
+    pass
+
+class RPI4ModelB:
+    pass
+
+if __name__ == "main":
+    main()

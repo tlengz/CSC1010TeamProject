7ab3a1960217a78a83d1491d272181a3a0247cec
KeeBoonHwee <2717921k@student.gla.ac.uk>
2022-03-20 23:37:19 +0800

    draft versions

--- Assignment_2/KBH_ChatClient.py
+++ Assignment_2/KBH_ChatClient.py
@@ -2,41 +2,29 @@ import socket
 import threading
 import queue
 
-class ChatServer:
+class ChatClient:
     #-----------------------------------------------------------------------------------------------
     def __init__(self, userName, portNumber=7):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        self.ip = s.getsockname()[0]
         self.port = int(portNumber)                                            # RFC 862 echo protocol states TCP port = 7
-        s.close()
         self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-        self.server_address = ( self.ip, self.port )
-        self.socket.bind(self.server_address)
-        print('Chat Server on ipv4 {}, is listening on port {}...'.format(*self.server_address))
+        self.server_address = ( "192.168.0.124", self.port )
+        self.socket.connect(self.server_address)
+        print('Chat Client on ipv4 {}, is listening on port {}...'.format(*self.server_address))
         self.CMD_LIST     = "LIST images"
         self.CMD_DOWNLOAD = "DOWNLOAD image.jpg"
         self.quitFlag = False
         self.userName = userName
-        self.clientCount = 0
-        self.clients = {}
         self.messageQueue = queue.Queue()
         self.echoThread   = threading.Thread(target = self.Echo)
         self.echoThread.start()
         self.inputThread = threading.Thread(target = self.Input)
         self.inputThread.start()
-        self.listenThread = threading.Thread(target = self.Listen)
+        self.listenThread = threading.Thread(target = self.Receiver)
         self.listenThread.start()
         self.echoThread.join()
         self.inputThread.join()
         self.listenThread.join()
-    #-----------------------------------------------------------------------------------------------
-    def Listen(self):
-        self.socket.listen(1)
-        while(not self.quitFlag):
-            client = threading.Thread(target = self.AcceptConnection)
-            client.start()
-            client.join()
     #-----------------------------------------------------------------------------------------------
     def Echo(self):
         while(not self.quitFlag):
@@ -45,35 +33,27 @@ class ChatServer:
                 if(message[0] == self.userName):
                     msg = message[0] + " > " + message[1]
                     msg = bytearray( msg , 'UTF-8')
-                    for id, cn in self.clients.items():
-                        cn.sendall( msg )
+                    self.socket.sendall( msg )
                 else:
                     print("\n" + message[1] )
-
-
     #-----------------------------------------------------------------------------------------------
     def Input(self):
         while(not self.quitFlag):
             yourmessage = input("Me > ")
             self.messageQueue.put( (self.userName , yourmessage) )
     #-----------------------------------------------------------------------------------------------
-    def AcceptConnection(self):
-        connection, client_address = self.socket.accept()
-        self.clientCount += 1
-        self.clients[ str(client_address[1]) + str(self.clientCount) ] = connection
+    def Receiver(self):
         try:
-            while(True):
-                data = connection.recv(64)           # This must be the same as the client, and vice versa
+            while(not self.quitFlag):
+                data = self.socket.recv(64)           # This must be the same as the client, and vice versa
                 if data:
-                    self.messageQueue.put( (str(client_address[1]), bytes(data).decode('UTF-8') ) )
+                    self.messageQueue.put( (str("Server"), bytes(data).decode('UTF-8') ) )
         except Exception as e:
-            connection.close()
-            self.clients.pop(client_address)
             print("\n" + str(e))
     #-----------------------------------------------------------------------------------------------
     def __del__(self):
         self.socket.close()
-        print("\nCharServer has terminated.\n")
+        print("\nCharClient has terminated.\n")
     #-----------------------------------------------------------------------------------------------
 
 def main():
@@ -87,12 +67,12 @@ def main():
             except:
                 pass
         name = input("Please enter your chat moniker : ")
-        server = ChatServer(name,port)
+        client = ChatClient(name,port)
 
     except Exception as e:
-        if server is not None:
-            server.close()
-            server = None
+        if client is not None:
+            client.close()
+            client = None
         print(str(e))
 
 if __name__ == "__main__":

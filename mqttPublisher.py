a1721278ed777440310ad660d2bfbb412ebed8ae
KeeBoonHwee <2101797@sit.singaporetech.edu.sg>
2022-03-28 01:16:26 +0800

    mqtt publisher

---
+++ mqttPublisher.py
@@ -0,0 +1,5 @@
+#import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+#publish.single(topic="test", payload="Hello", hostname="localhost",port=1883)
+publish.single(topic="test", payload="Hello", retain=True, hostname="192.168.0.157",port=1883,keepalive=5,)
